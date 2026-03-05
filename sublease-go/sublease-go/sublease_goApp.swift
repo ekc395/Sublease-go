76eb477e97fe48ebeb53bf955071b4a59c4d5dff
ekc395 <ekc395@gmail.com>
2026-03-05 12:13:29 -0800

    Firebase connection

--- sublease-go/sublease-go/sublease_goApp.swift
+++ sublease-go/sublease-go/sublease_goApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct sublease_goApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
